819451da1195b2dd6ed80e5a7d8bb909bb57e47f
Josh r <jstwrenn@gmail.com>
2019-02-27 23:21:22 -0500

    Add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -31,6 +31,31 @@ app.get('/todos', (req, res) => {
     });
 });
 
+//GET /todos/{id}
+app.get('/todos/:id', (req, res) => {
+    var objectId = require('mongodb').ObjectID;
+    var id = req.params.id;
+
+    if(!objectId.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
+        return res.status(400).send();
+    });
+    //Have a valid id - if not pass back 404 send back empy boody
+    //findbyid 
+        //Success
+            //if todo - send it back 
+            //if no todo - send back 404 with empty body
+        //error
+            //400 - request was not valid - going to send empty body
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
